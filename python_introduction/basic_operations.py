68d89273495bc5a2ce13a4ad4cda1cb4685cf7ab
Ahmed Mohamed <amali20253@gmail.com>
2025-05-26 19:23:12 +0300

    Update basic_operations.py

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,9 @@
 ["number1 = 10", "number2 = 5"]
-
+number1= 10
+number2= 5
+r1= number1 + number2 
+r2 = number1 - number2 
+r3 = number1 * number2 
 
 print ("addiction of 10 and 5 is 15")
 print ("substraction of 10 and 5 is 5")
