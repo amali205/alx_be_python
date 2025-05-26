7f0618f5255f3680d02f95d765c9c79a18336ad9
am8899 <amali20253@gmail.com>
2025-05-27 02:19:01 +0300

    advanced was add

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+i=int (input("enter your monthly incom : "))
+ib= int(input("enter your totaly monthly expenses :"))
+sav= i - ib 
+prosav = sav*12 + sav*12*0.05
+
+print (i)
+print (ib)5
+print (sav)
+print ( prosav)
