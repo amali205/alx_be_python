d8c50eb85dd1917a5688b94edc3c319446a0ecc6
am8899 <amali20253@gmail.com>
2025-05-27 02:41:39 +0300

    oh

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 i=int (input("Enter your monthly income:"))
 ib= int(input("Enter your total monthly expenses:"))
-sav= i - ib 
-prosav = sav*12 + sav*12*0.05
+sav= i- ib
+prosav = sav*12 +int( sav*12*0.05)
 
 print (i)
 print (ib)
 print (sav)
-print (float(prosav))
+print (prosav)
