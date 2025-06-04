89ff96ab25654cc68e24eea0ecab922c1a3f9b0d
am8899 <amali20253@gmail.com>
2025-06-04 16:29:38 +0300

    i edit the fileesxfxzaaaaaa

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,7 +2,7 @@ number=int(input("Enter a number to see its multiplication table: "))
 x= number 
 for y in range (1,11):
  z= x * y
- print(f"{x}*{y}={z}")
+ print(f" {x} * {y} = {z} ")
         
     
     
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the size of the pattern:"))
+i = 0
+while a in range (1, a+1):
+    i+=1
+    for a in range (1, a+1):
+     print ("*" , end="")
+    print ()
+    if i == a : 
+     break
+
+
+
+
+
+
+    
