078065e1936f86cdaa9a15f328cb7b86c911f22d
am8899 <amali20253@gmail.com>
2025-06-12 21:30:13 +0300

    new folder addddd

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,19 @@
-["num1, num2, operation"]
-def perform_operation(num1 , *num2 , **operation):
+def perform_operation(num1 , num2 , operation):
     if operation == 'add': 
-        print(float(num1) + float(num2))
+        return num1 + num2
     elif operation == 'subtract':
-         print(float(num1) - float(num2))
+         return num1 - num2
     elif operation == 'multiply':
-         print(float(num1) * float(num2))
-    if operation == 'divide':
-      print(float(num1) / float(num2))
-
-print("Arithmetic Operations")    
-perform_operation(float(input("Enter the first number:")), float(input("Enter the second number:")),  str(input("Enter the operation (add, subtract, multiply, divide):")))  
+         return num1 * num2
+    elif operation == 'divide':
+         if num2 == 0: 
+           return "Error: Division by zero"
+         else:
+             return num1 / num2
+    else :
+         return "Error: Unknown operation"
+         
+      
+  
     
 
