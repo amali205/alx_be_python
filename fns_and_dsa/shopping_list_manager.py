8e01ca095dbfaecf45a8e2cc338460a5641e7779
am8899 <amali20253@gmail.com>
2025-06-12 20:53:17 +0300

    new folder addd

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,17 @@
+def perform_operation(num1 , num2 , operation):
+    if operation == 'add': 
+        print(float(num1) + float(num2))
+    elif operation == 'subtract':
+         print(float(num1) - float(num2))
+    elif operation == 'multiply':
+         print(float(num1) * float(num2))
+    elif operation == 'divide':
+      print(float(num1) / float(num2))
+    else:
+        if num2 == 0 and operation == "divide":
+            print("eror")
+
+    
+perform_operation(float(input("enter num1 :")), float(input("enter num2 :")),  input("chose the operation ['add', 'subtract', 'multiply', 'divide']"))  
+    
+
