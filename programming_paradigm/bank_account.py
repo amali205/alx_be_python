80cc6f09e58eb31cd55654c5df92f0680a631773
am8899 <amali20253@gmail.com>
2025-06-29 01:13:04 +0300

    addd

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount :
+    def __init__(self , intial_balance=0):
+        self.account_balance = intial_balance
+    def deposit(self ,amount):
+         self.account_balance += amount 
+    def withdraw(self, amount):
+         if amount <= self.account_balance:
+          self.account_balance -= amount
+          return True
+         return False
+    def display_balance(self):
+      print (f"The balance of account is {self.account_balance}")
+      
+        
+            
