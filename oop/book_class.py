32be10ea5552dd54c9c9d833a004d8b5cc258502
am8899 <amali20253@gmail.com>
2025-06-29 02:57:41 +0300

    hh

---
+++ oop/book_class.py
@@ -0,0 +1,24 @@
+class Book :
+    def __init__(self, title, author, year):
+        self.title=title
+        self.author=author
+        self.year=year
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+
+
+
+    def __del__(self):
+        print (f"Deleting {self.title}")
+
+s = Book("name" , "ahmed" , 2000)
+
+print (s) 
+print (repr(s)) 
+
+del Book
+
+
