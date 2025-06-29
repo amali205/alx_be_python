55625c33748392e96f50c108c20d09adc2690c53
am8899 <amali20253@gmail.com>
2025-06-29 21:34:10 +0300

    hh

--- oop/library_system.py
+++ oop/library_system.py
@@ -20,7 +20,7 @@ class PrintBook(Book):
 
 class Library:
    def __init__(self):
-     self.books= []
+     self.books = []
    def add_book(self, book):
      self.books.append(book)
    def list_books(self):
