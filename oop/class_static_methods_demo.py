649407668c61feeec076085778299af6e4e52b95
am8899 <amali20253@gmail.com>
2025-06-29 03:22:04 +0300

    dd

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -3,8 +3,8 @@ class Calculator:
     @staticmethod
     def add(a, b):
         return a + b
-    def multiply(a, b):
-         print(f"Calculation type: {Calculator.calculation_type}")
+    def multiply(cls,a, b):
+         print(f"Calculation type: {cls.calculation_type}")
          return a * b
 
 
